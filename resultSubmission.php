234307f9fe3bfd4c9bcb48aa78cce90faf0632c1
MikasaNT <waiyan01101994@gmail.com>
2016-07-27 01:02:21 +0800

    mmm

--- resultSubmission.php
+++ resultSubmission.php
@@ -13,12 +13,15 @@ if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 echo "Connected successfully";
+echo '<script>';
+echo 'console.log("ayylmao")';
+echo '</script>';
 
 //Select database to use
 mysqli_select_db($conn,"capstone");
 
 //Arrange an SQL statement
-$sql = "INSERT INTO capstone.capstonespeedtest VALUES (default, $_POST[download], $_POST[upload], $_POST[latency], $_POST[jitter], $_POST[testServer], $_POST[ip], $_POST[hostname], NOW());";
+$sql = "INSERT INTO capstone.capstonespeedtest VALUES (default, 1, 1, 1, 1, 'test', 'test', 'test', NOW());";
 
 //Query using the arranged SQL statment and retrieve the result from the DB.
 $result = mysqli_query($conn, $sql);
